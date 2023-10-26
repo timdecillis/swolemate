b2bdb0e942343f520d3baf1a5a142823f8e1aa9e
Tim DeCillis <timdecillis@gmail.com>
2023-10-25 18:48:15 -0700

    Added package.json

--- server/app.py
+++ server/app.py
@@ -1,1 +1,13 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    print('Visited localhost:3000')
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.debug = True
+    print('Starting the Flask server')
+    app.run(port=3000)
