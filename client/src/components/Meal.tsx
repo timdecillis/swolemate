9b9224eb383c8192fe081218576eed1f4dcfac4f
Tim DeCillis <timdecillis@gmail.com>
2023-11-01 18:07:41 -0700

    Added form to submit new meal items

--- client/src/components/Meal.tsx
+++ client/src/components/Meal.tsx
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 
 interface MealProps {
   label: string;
 }
 
-const Meal = ({ label }: MealProps ) => {
+const Meal = ({ label }: MealProps) => {
 
-  const [mealItems, setMealItems] = useState(['cheese', 'soda']);
+  const [mealItems, setMealItems] = useState<string[]>([]);
+  const [input, setInput] = useState('');
+
+  const onSubmit = (event: SyntheticEvent) => {
+    event.preventDefault()
+    setMealItems([...mealItems, input]);
+  }
 
   return (
     <>
@@ -14,7 +20,11 @@ const Meal = ({ label }: MealProps ) => {
       <div>
         {mealItems.map(item => <div>{item}</div>)}
       </div>
-      <input></input>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="new-item">Add a new {label} item: </label><br/>
+        <input type="text" id="new_item" name="new_item" onChange={e => setInput(e.target.value)}></input>
+        <input type="submit" value="add it!"/>
+      </form>
     </>
   )
 }
