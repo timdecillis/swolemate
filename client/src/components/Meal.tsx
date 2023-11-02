80483bc7bcdf1470c6be5b5a1103b0f15f7aa6ee
Tim DeCillis <timdecillis@gmail.com>
2023-11-01 17:23:49 -0700

    Added useState to Meal

--- client/src/components/Meal.tsx
+++ client/src/components/Meal.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface MealProps {
   label: string;
 }
 
 const Meal = ({ label }: MealProps ) => {
+
+  const [mealItems, setMealItems] = useState(['cheese', 'soda']);
+
   return (
     <>
       <div>{label}</div>
+      <div>
+        {mealItems.map(item => <div>{item}</div>)}
+      </div>
       <input></input>
     </>
   )
