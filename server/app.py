13f00a337622c47148defd26a202a512e2ae7fa7
Tim DeCillis <timdecillis@gmail.com>
2023-11-05 10:14:36 -0800

    Updated single quotes to doubles

--- server/app.py
+++ server/app.py
@@ -3,22 +3,22 @@ from flask import Flask, request
 app = Flask(__name__)
 
 foods = {
-    'breakfast': ['cereal', 'eggs', 'bacon', 'fruit'],
-    'lunch': ['sandwich', 'soup', 'salad'],
-    'dinner': ['spaghetti', 'chicken', 'waffles']
+    "breakfast": ["cereal", "eggs", "bacon", "fruit"],
+    "lunch": ["sandwich", "soup", "salad"],
+    "dinner": ["spaghetti", "chicken", "waffles"]
 }
 
 
 @app.after_request
 def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
     return response
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Hello, Swolemate!'
+    return "Hello, Swolemate!"
 
 @app.route('/test')
 def test():
