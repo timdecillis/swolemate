3f43bea46458d7559d2c9d9650e9d86291eaeda6
Tim DeCillis <timdecillis@gmail.com>
2023-11-15 16:52:34 -0800

    Added basic EditForm with inputs

--- client/src/components/EditForm.tsx
+++ client/src/components/EditForm.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 
 interface EditFormProps {
   editOpen: boolean;
+  template: string;
 }
 
-const EditForm = ({ editOpen }: EditFormProps) => {
+const EditForm = ({ editOpen, template }: EditFormProps) => {
 
-  if(!editOpen) return null
+  if (!editOpen) return null
 
   return (
     <div>
-
-  </div>
+      <h3>Please edit your template</h3>
+      <form>
+        <input type='text' value={template}/>
+        <input type='submit' value='Save' />
+      </form>
+    </div>
   )
 }
 
--- client/src/components/Template.tsx
+++ client/src/components/Template.tsx
@@ -1,4 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+
+import EditForm from './EditForm';
 
 interface TemplateProps {
   index: number;
@@ -10,17 +12,20 @@ const Template = ({ template, index, deleteTemplate }: TemplateProps) => {
 
   const [editOpen, setEditOpen] = useState<boolean>(false);
 
-  if(editOpen) return null
+  if (editOpen) {
+    return <EditForm editOpen={editOpen} template={template} />
+  } else {
+    return (
+      <div key={index}>
+        <h3>{index + 1}.) {template}</h3>
+        <button onClick={() => setEditOpen(true)} >Edit</button>
+        <button onClick={() => {
+          deleteTemplate(template)
+        }} >X</button>
+      </div>
+    )
+  }
 
-  return (
-    <div key={index}>
-    <h3>{index + 1}.) {template}</h3>
-    <button onClick={() => setEditOpen(true)} >Edit</button>
-    <button onClick={() => {
-      deleteTemplate(template)
-    }} >X</button>
-  </div>
-  )
 }
 
 export default Template
