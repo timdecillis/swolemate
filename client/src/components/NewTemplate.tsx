511d29b8161c6026415f2c637664038864243ecd
Tim DeCillis <timdecillis@gmail.com>
2023-11-18 19:04:31 -0800

    Added insert variable button

--- client/src/components/NewTemplate.tsx
+++ client/src/components/NewTemplate.tsx
@@ -10,6 +10,7 @@ const NewTemplate = ({ user, setNewTemplateOpen }: NewTemplateProps) => {
   return (
     <>
       <h2> </h2>
+      <button>Insert variable</button>
       <button onClick={() => setNewTemplateOpen(false)} >Discard</button>
       <button>Save</button>
       <h2> </h2>
