5c06e570b7a5716a2a9db87ac34652037c1254a9
Tim DeCillis <timdecillis@gmail.com>
2023-11-28 23:41:41 -0800

    Completed error message for adding text to template

--- client/src/components/TemplateEditor.tsx
+++ client/src/components/TemplateEditor.tsx
@@ -15,18 +15,21 @@ const TemplateEditor = ({ setNewTemplateOpen, template, editTemplateString, addN
 
   const [variableOpen, setVariableOpen] = useState<boolean>(false);
   const [input, setInput] = useState<string>('');
+  const [errorOpen, setErrorOpen] = useState<boolean>(false);
 
   return (
     <>
       <h4>Enter text: </h4>
-      <form onSubmit={(e: SyntheticEvent) => {
+      <form onClick={() => setErrorOpen(false)} onSubmit={(e: SyntheticEvent) => {
         e.preventDefault();
+        if (!input) setErrorOpen(true);
         editTemplateString(input);
         setInput('');
       }} >
         <input value={input} onChange={e => setInput(e.target.value)} type='text' ></input>
         <button type='submit'>Add to template</button>
       </form>
+      {errorOpen && <div>Please enter some text to add!</div>}
       <h4> </h4>
       {variableOpen && <AddVariable addExistingVariableToString={addExistingVariableToString} template={template} setVariableOpen={setVariableOpen} addNewVariable={addNewVariable}/>}
       <button onClick={() => setVariableOpen(true)} >Insert variable</button>
