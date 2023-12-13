2beab5a33b021dba501b1c130ec5651939eb33fc
Tim DeCillis <timdecillis@gmail.com>
2023-12-12 18:23:02 -0800

    Added store

---
+++ client/src/App/store.ts
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// import typesReducer from './components/HairType/typesSlice'
+
+export default configureStore({
+  reducer: {
+    // types: typesReducer,
+  }
+})
