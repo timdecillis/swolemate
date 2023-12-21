81b33e5e81408cb624dd227424c89f032f6c1d4f
Tim DeCillis <timdecillis@gmail.com>
2023-12-20 19:33:40 -0800

    moved save to helpers

--- client/src/Utilities/helpers.ts
+++ client/src/Utilities/helpers.ts
@@ -8,24 +8,33 @@ const instance = axios.create({
 
 
 export const updateTemplate = (oldValue: string, newValue: string, user: string) => {
-    instance.put('/updateTemplate', { oldValue, newValue, user })
-      .then(({ data }) => {
-        // setTemplates(data)
-      })
-  }
+  instance.put('/updateTemplate', { oldValue, newValue, user })
+    .then(({ data }) => {
+      // setTemplates(data)
+    })
+}
 
-  export const deleteTemplate = (id: string, user: string | null) => {
-    instance.delete('/deleteTemplate', { data: { id, user } })
-      .then(({ data }) => {
-        // setTemplates(data);
-      })
-  }
+export const deleteTemplate = (id: string, user: string | null) => {
+  instance.delete('/deleteTemplate', { data: { id, user } })
+    .then(({ data }) => {
+      // setTemplates(data);
+    })
+}
 
-  export const renderString = (template: TemplateType) => {
-    return template.string.map((part: (string | string[])) => {
-      if (Array.isArray(part)) {
-        return template.variables[part[0]];
-      }
-      return part;
-    }).join(' ');
-  }
+export const renderString = (template: TemplateType) => {
+  return template.string.map((part: (string | string[])) => {
+    if (Array.isArray(part)) {
+      return template.variables[part[0]];
+    }
+    return part;
+  }).join(' ');
+}
+
+export const saveNewTemplate = (user, template) => {
+  instance.post('/addTemplate', { user, template })
+    .then(({ data }) => {
+      console.log('DATA FROM API:', data)
+      setTemplates(data);
+      setNewTemplateOpen(false);
+    })
+}
