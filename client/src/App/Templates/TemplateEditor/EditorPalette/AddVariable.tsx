f6370ce32ee3fe75e4657b17c8d4e04122c3e1b5
Tim DeCillis <timdecillis@gmail.com>
2023-12-26 20:13:48 -0800

    Updated adding variables to template in local state

--- client/src/App/Templates/TemplateEditor/EditorPalette/AddVariable.tsx
+++ client/src/App/Templates/TemplateEditor/EditorPalette/AddVariable.tsx
@@ -1,17 +1,35 @@
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { SetStateAction, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { setNewVariableOpen } from '../../templatesSlice';
-import { getNewTemplate, addNewVariable, addExistingVariable } from '../newTemplateSlice';
+import { TemplateType } from '../newTemplateSlice';
 
-const AddVariable = () => {
+type AddVariableProps = {
+  template: TemplateType;
+  setTemplate: React.Dispatch<SetStateAction<TemplateType>>;
+}
+
+const AddVariable = ({ template, setTemplate }: AddVariableProps) => {
 
   const dispatch = useDispatch();
-  const template = useSelector(getNewTemplate);
 
   const [name, setName] = useState<string>('');
   const [content, setContent] = useState<string>('');
   const [errorOpen, setErrorOpen] = useState<boolean>(false);
+
+  const addExistingVariable = (name: string) => {
+    console.log('adding existing', template.string)
+    let prevString = template.string;
+    prevString.push([name]);
+    setTemplate({ ...template, string: prevString });
+  }
+
+  const addNewVariable = () => {
+    let prevString = template.string;
+    let prevVariables = template.variables;
+    prevString.push([name]);
+    setTemplate({ ...template, string: prevString, variables: { ...template.variables, [name]: content } })
+  }
 
   return (
     <>
@@ -23,9 +41,9 @@ const AddVariable = () => {
               <div key={i} >
                 <div>Name: {tuple[0]} Content: {tuple[1]}</div>
                 <button onClick={() => {
-                  dispatch(addExistingVariable({name: tuple[0]}));
-                  dispatch(setNewVariableOpen({condition: false}));
-                  }} >Insert</button>
+                  addExistingVariable(tuple[0]);
+                  dispatch(setNewVariableOpen({ condition: false }));
+                }} >Insert</button>
               </div>
             );
           })}
@@ -33,14 +51,14 @@ const AddVariable = () => {
         </>
       )}
       <h4>Create a new variable:</h4>
-      <input onClick={() => setErrorOpen(false)} onChange={(e) => {setName(e.target.value)}} placeholder='Variable name' ></input>
+      <input onClick={() => setErrorOpen(false)} onChange={(e) => { setName(e.target.value) }} placeholder='Variable name' ></input>
       <input onClick={() => setErrorOpen(false)} onChange={(e) => setContent(e.target.value)} placeholder='Variable content' ></input>
       {errorOpen && <div>Please enter a variable name and content!</div>}
-      <button onClick={() => dispatch(setNewVariableOpen({condition: false}))} >Discard</button>
+      <button onClick={() => dispatch(setNewVariableOpen({ condition: false }))} >Discard</button>
       <button onClick={() => {
         if (!name || !content) return setErrorOpen(true);
-        dispatch(addNewVariable({name, content}));
-        dispatch(setNewVariableOpen({condition: false}));
+        addNewVariable();
+        dispatch(setNewVariableOpen({ condition: false }));
       }} >Add to template</button>
     </>
   )
