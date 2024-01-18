8a05ba3b06c5beda742fec82c6d432c968ba545f
Tim DeCillis <timdecillis@gmail.com>
2024-01-17 18:30:09 -0800

    changed position cancel name button

--- client/src/App/Templates/TemplateEditor/AddName.tsx
+++ client/src/App/Templates/TemplateEditor/AddName.tsx
@@ -1,48 +1,56 @@
-import React, { useState, SyntheticEvent, SetStateAction } from 'react';
+import React, { useState, SyntheticEvent, SetStateAction } from "react";
 
-import { setNewTemplateOpen } from '../templatesSlice';
-import { TemplateType } from './newTemplateSlice';
-import { useDispatch } from 'react-redux';
+import { setNewTemplateOpen } from "../templatesSlice";
+import { TemplateType } from "./newTemplateSlice";
+import { useDispatch } from "react-redux";
 
 type AddNameProps = {
   setAddNameOpen: React.Dispatch<SetStateAction<boolean>>;
   setPaletteOpen: React.Dispatch<SetStateAction<boolean>>;
   setTemplate: React.Dispatch<SetStateAction<TemplateType>>;
   template: TemplateType;
-}
+};
 
-const AddName = ({ setAddNameOpen, setPaletteOpen, setTemplate, template }: AddNameProps) => {
-
+const AddName = ({
+  setAddNameOpen,
+  setPaletteOpen,
+  setTemplate,
+  template,
+}: AddNameProps) => {
   const dispatch = useDispatch();
 
-  const [input, setInput] = useState<string>('');
+  const [input, setInput] = useState<string>("");
   const [errorOpen, setErrorOpen] = useState<boolean>(false);
 
   const cancel = () => {
     setAddNameOpen(false);
-    dispatch(setNewTemplateOpen({condition: false}));
-  }
+    dispatch(setNewTemplateOpen({ condition: false }));
+  };
 
-  const onSubmit = ((e: SyntheticEvent) => {
+  const onSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
     if (!input) return setErrorOpen(true);
-    setTemplate({...template, name:input})
-    setAddNameOpen(false)
-    setPaletteOpen(true)
-  })
+    setTemplate({ ...template, name: input });
+    setAddNameOpen(false);
+    setPaletteOpen(true);
+  };
 
   return (
     <>
       <form onSubmit={onSubmit}>
         <h3>Please enter a name for the template:</h3>
-        <input onClick={() => setErrorOpen(false)} onChange={e => setInput(e.target.value)} type='text'></input>
-        <input type='submit' value='Save'></input>
+        <input
+          onClick={() => setErrorOpen(false)}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+        ></input>
+        <input type="submit" value="Save"></input>
+        <button onClick={cancel}>Cancel</button>
       </form>
       {errorOpen && <div>Please enter a name for your template!</div>}
-      <button onClick={cancel} >Cancel</button>
       <h1> </h1>
     </>
-  )
-}
+  );
+};
 
-export default AddName
+export default AddName;
