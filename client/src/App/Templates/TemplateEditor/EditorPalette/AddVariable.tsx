71d535cdf4342c54f1b6931463aed9bca763601e
timdecillis <timdecillis@gmail.com>
2024-05-24 18:44:52 -0700

    converted AddVariable to form

--- client/src/App/Templates/TemplateEditor/EditorPalette/AddVariable.tsx
+++ client/src/App/Templates/TemplateEditor/EditorPalette/AddVariable.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction, useState } from "react";
+import { SetStateAction, SyntheticEvent, useState } from "react";
 
 import { TemplateType } from "../newTemplateSlice";
 
@@ -29,6 +29,13 @@ const AddVariable = ({
     });
   };
 
+  const handleSubmit = (e:SyntheticEvent) => {
+    e.preventDefault();
+    if (!name || !content) return setErrorOpen(true);
+    addNewVariable();
+    setNewVariableOpen(false);
+  };
+
   return (
     <>
       <ExistingVariableChoices
@@ -37,7 +44,7 @@ const AddVariable = ({
         setTemplate={setTemplate}
       />
       <h4>Create a new variable:</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           onClick={() => setErrorOpen(false)}
           onChange={(e) => {
@@ -50,18 +57,10 @@ const AddVariable = ({
           onChange={(e) => setContent(e.target.value)}
           placeholder="Variable content"
         ></input>
+        <input type='submit' value='Add to Template'></input>
         {errorOpen && <div>Please enter a variable name and content!</div>}
         <button onClick={() => setNewVariableOpen(false)}>Cancel</button>
       </form>
-      <button
-        onClick={() => {
-          if (!name || !content) return setErrorOpen(true);
-          addNewVariable();
-          setNewVariableOpen(false);
-        }}
-      >
-        Add to template
-      </button>
     </>
   );
 };
