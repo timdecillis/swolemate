3832b7f4b01ee849f9902936deb3e2762dc2b0ff
timdecillis <timdecillis@gmail.com>
2024-05-28 15:49:15 -0700

    updated formatting

--- client/src/App/Templates/TemplateEditor/EditVariable.tsx
+++ client/src/App/Templates/TemplateEditor/EditVariable.tsx
@@ -1,7 +1,7 @@
-import { useState, SyntheticEvent, SetStateAction } from 'react';
+import { useState, SyntheticEvent, SetStateAction } from "react";
 
-import { TemplateType } from './newTemplateSlice';
-import Button from '../../Button';
+import { TemplateType } from "./newTemplateSlice";
+import Button from "../../Button";
 
 interface EditVariableProps {
   setEditVariableOpen: React.Dispatch<SetStateAction<boolean>>;
@@ -10,16 +10,20 @@ interface EditVariableProps {
   template: TemplateType;
 }
 
-const EditVariable = ({ setEditVariableOpen, variable, setTemplate, template }: EditVariableProps) => {
-
-  const [variableName, setVariableName] = useState<string>('');
-  const [variableContent, setVariableContent] = useState<string>('');
+const EditVariable = ({
+  setEditVariableOpen,
+  variable,
+  setTemplate,
+  template,
+}: EditVariableProps) => {
+  const [variableName, setVariableName] = useState<string>("");
+  const [variableContent, setVariableContent] = useState<string>("");
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
-    let prevName = '';
-    let name = '';
-    let content = '';
+    let prevName = "";
+    let name = "";
+    let content = "";
     if (!variableContent) {
       prevName = variable[0];
     }
@@ -34,30 +38,38 @@ const EditVariable = ({ setEditVariableOpen, variable, setTemplate, template }: 
       content = variableContent;
     }
     let string = [...template.string];
-    let variables = {...template.variables}
+    let variables = { ...template.variables };
     if (prevName) {
-      string.forEach((item: (string | string[])) => {
+      string.forEach((item: string | string[]) => {
         if (Array.isArray(item) && item[0] === prevName) item[0] = name;
-      })
+      });
       delete variables[prevName];
       variables[name] = content;
     } else {
       variables[name] = content;
     }
-    setTemplate({...template, string, variables});
+    setTemplate({ ...template, string, variables });
     setEditVariableOpen(false);
-  }
+  };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input onChange={(e) => setVariableName(e.target.value)} type='text' defaultValue={variable[0]}></input>
-        <input onChange={(e) => setVariableContent(e.target.value)} type='text' defaultValue={variable[1]}></input>
-        <button type='submit'>Save</button>
+        <input
+          onChange={(e) => setVariableName(e.target.value)}
+          type="text"
+          defaultValue={variable[0]}
+        ></input>
+        <input
+          onChange={(e) => setVariableContent(e.target.value)}
+          type="text"
+          defaultValue={variable[1]}
+        ></input>
+        <button type="submit">Save</button>
       </form>
-      <Button handler={() => setEditVariableOpen(false)} content='Cancel'/>
+      <Button handler={() => setEditVariableOpen(false)} content="Cancel" />
     </>
-  )
-}
+  );
+};
 
-export default EditVariable
+export default EditVariable;
