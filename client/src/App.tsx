cb0abf4f3f8832b5a2c03b7d3d80f243d026faa4
timdecillis <timdecillis@gmail.com>
2024-07-11 16:12:01 -0700

    added mui Input

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ import TemplateEditor from "./App/TemplateEditor";
 import { getSignedIn, getUser } from "./App/userSlice";
 import { getLoading, getNewTemplateOpen } from "./App/Templates/templatesSlice";
 import { useCustomDispatch } from "./Utilities/handlers";
-import { Button } from "@mui/material";
+import { Button, Input } from "@mui/material";
 
 function App() {
   const loading = useSelector(getLoading);
@@ -44,6 +44,7 @@ function App() {
             <SignIn />
           )} */}
         </div>
+        <Input/>
         <Button variant="contained">Hello</Button>
       </div>
     </div>
